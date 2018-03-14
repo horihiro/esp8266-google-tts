9f45178f186041eb522fb5c1f468d3cd34b9a5ec
horihiro <hori.kawa.shima.tool+github@gmail.com>
2018-03-15 06:13:53 +0900

    first commit.

--- examples/SimpleUsage/SimpleUsage.ino
+++ examples/SimpleUsage/SimpleUsage.ino
@@ -1,28 +1,43 @@
-# esp8266-google-tts
-Simple Google TTS Library for esp8266.
-
-This library generates the link to speech mp3 file from text using Google Translate Service.
-
-
-## Usage
-
-```
 #include <google-tts.h>
 #include <ESP8266WiFi.h>
 
 const char* ssid     = "<REPLASE_YOUR_WIFI_SSID>";
 const char* password = "<REPLASE_YOUR_WIFI_PASSWORD>";
 
+String getMacAddress() {
+  byte mac[6];
+
+  WiFi.macAddress(mac);
+  String cMac = "";
+  for (int i = 0; i < 6; ++i) {
+    if (mac[i] < 0x10) {
+      cMac += "0";
+    }
+    cMac += String(mac[i], HEX);
+    if (i < 5)
+      cMac += ":"; // put : or - if you want byte delimiters
+  }
+  cMac.toUpperCase();
+  return cMac;
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("");
+//  Serial.println(String(b>>7));
   WiFi.mode(WIFI_STA);  WiFi.begin(ssid, password);
-
+  Serial.print("MAC: ");
+  Serial.println(getMacAddress());
   while (WiFi.status() != WL_CONNECTED) {
     delay(250);
     Serial.print(".");
   }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
   TTS tts;
   Serial.println(tts.getSpeechUrl("こんにちは、世界！", "ja"));
@@ -34,4 +49,3 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
-```
---
+++ google-tts.h
@@ -0,0 +1,29 @@
+#ifndef GoogleTTS_h
+#define GoogleTTS_h
+
+#include <WiFiClientSecure.h>
+
+#define LIB_NAME "GoogleTTS for ESP8266"
+#define LIB_VERSION "0.1"
+
+#define HOST_GTRANS "translate.google.com"
+#define PATH_GTRANS "/translate_tts"
+#define FINGERPRINT_GTRANS "51:95:BF:29:CC:79:4B:FE:E2:0B:0F:EA:B5:95:1B:5A:0A:91:AB:39"
+
+typedef class GoogleTTS {
+
+private:  
+  String getTKK();
+  String createToken(const char* text, const char* key);
+  String urlencode(String str);
+  char* lltoa(long long val, int base);
+  long long int  XL (long long int a, char* b);
+  
+public:
+  String getSpeechUrl(String text, String lang);
+  String getSpeechUrl(String text) {
+    return getSpeechUrl(text, "en");
+  }
+} TTS;
+
+#endif
